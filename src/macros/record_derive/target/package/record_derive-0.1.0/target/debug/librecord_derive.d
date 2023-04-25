d39c57cda845540039751da53900fbba1c3af2a6
Isaac Newton Dzikum <nisaacdz@gmail.com>
2023-04-25 01:50:27 +0000

    updated toml

---
+++ src/macros/record_derive/target/package/record_derive-0.1.0/target/debug/librecord_derive.d
@@ -0,0 +1,1 @@
+/workspaces/blockify/src/macros/record_derive/target/package/record_derive-0.1.0/target/debug/librecord_derive.so: /workspaces/blockify/src/macros/record_derive/target/package/record_derive-0.1.0/src/lib.rs
