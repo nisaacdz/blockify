f2afef8fc5ecca12e698fc225c36386dc8e863bc
Isaac Newton Dzikum <nisaacdz@gmail.com>
2023-04-27 02:22:50 +0000

    updated where clause

--- src/macros/record_derive/src/lib.rs
+++ src/macros/record_derive/src/lib.rs
@@ -1,25 +1,24 @@
-///! # Record - **record_derive**
-///! 
-///! This is a derive macro for the `Record` trait of `blockify` crate.
-///! This does not override any of the functions within the `Record` trait.
-///! 
-///! # Usage 
-///! ```
-///! use blockify::trans::record::Record;
-///! use serde::{Serialize, Deserialize}; 
-///! 
-///! #[derive(Debug, Clone, Serialize, Deserialize, Record)] 
-///! pub struct MarriageContract {
-///!   bride: String,
-///!   groom: String, 
-///! }
-///!
-///! #[derive(Serialize, Deserialize, Record)] 
-///! pub struct Detail<T> {
-///!   val: T 
-///! }
-///! ```
-
+//! # Record - **record_derive**
+//!
+//! This is a derive macro for the `Record` trait of `blockify` crate.
+//! This does not override any of the functions within the `Record` trait.
+//!
+//! # Usage
+//! ```
+//! use blockify::trans::record::Record;
+//! use serde::{Serialize, Deserialize};
+//!
+//! #[derive(Debug, Clone, Serialize, Deserialize, Record)]
+//! pub struct MarriageContract {
+//!   bride: String,
+//!   groom: String,
+//! }
+//!
+//! #[derive(Serialize, Deserialize, Record)]
+//! pub struct Detail<T> {
+//!   val: T
+//! }
+//! ```
 
 extern crate proc_macro;
 
@@ -37,10 +36,10 @@ pub fn record_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
 fn impl_record(input: &DeriveInput) -> proc_macro::TokenStream {
     let name = &input.ident;
     let generics = &input.generics;
-    let (impl_generics, ty_generics, _) = generics.split_for_impl();
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
     let gen = quote! {
-        impl #impl_generics Record for #name #ty_generics {
+        impl #impl_generics Record for #name #ty_generics #where_clause {
             // ...
         }
     };
---
+++ src/macros/record_derive/target/package/record_derive-0.2.1/target/debug/deps/record_derive-82a61763ea5e34f5.d
@@ -0,0 +1,5 @@
+/workspaces/blockify/src/macros/record_derive/target/package/record_derive-0.2.1/target/debug/deps/librecord_derive-82a61763ea5e34f5.so: src/lib.rs
+
+/workspaces/blockify/src/macros/record_derive/target/package/record_derive-0.2.1/target/debug/deps/record_derive-82a61763ea5e34f5.d: src/lib.rs
+
+src/lib.rs:
