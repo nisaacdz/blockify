eeefbe91af981349dc26dc9b194c818dffd14f35
Isaac Newton Dzikum <nisaacdz@gmail.com>
2023-04-27 01:24:31 +0000

    updated crate-level comments

--- src/macros/record_derive/src/lib.rs
+++ src/macros/record_derive/src/lib.rs
@@ -1,4 +1,25 @@
-// record_derive/src/lib.rs
+///! # Record - **record_derive**
+///! 
+///! This is a derive macro for the `Record` trait of `blockify` crate.
+///! This does not override any of the functions within the `Record` trait.
+///! 
+///! # Usage 
+///! ```
+///! use blockify::trans::record::Record;
+///! use serde::{Serialize, Deserialize}; 
+///! 
+///! #[derive(Debug, Clone, Serialize, Deserialize, Record)] 
+///! pub struct MarriageContract {
+///!   bride: String,
+///!   groom: String, 
+///! }
+///!
+///! #[derive(Serialize, Deserialize, Record)] 
+///! pub struct Detail<T> {
+///!   val: T 
+///! }
+///! ```
+
 
 extern crate proc_macro;
 
@@ -15,9 +36,13 @@ pub fn record_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
 
 fn impl_record(input: &DeriveInput) -> proc_macro::TokenStream {
     let name = &input.ident;
+    let generics = &input.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
     let gen = quote! {
-        impl Record for #name {}
+        impl #impl_generics Record for #name #ty_generics #where_clause {
+            // ...
+        }
     };
 
     gen.into()
