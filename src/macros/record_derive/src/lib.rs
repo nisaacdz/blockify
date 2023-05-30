66e49c35fc60ea0c5d08901c484357b08a883b93
nisaacdz <103804474+nisaacdz@users.noreply.github.com>
2023-05-30 00:12:34 +0000

    reimplementing Record derive

--- src/macros/record_derive/src/lib.rs
+++ src/macros/record_derive/src/lib.rs
@@ -1,8 +1,6 @@
 //! # Record - **record_derive**
 //!
-//! This is a derive macro for the `Record` trait of `blockify` crate.
-//! This does not override any of the functions within the `Record` trait.
-//!
+//! A derive macro for the `blockify::record::Record` crate.
 //! # Usage
 //! ```
 //! use blockify::trans::record::Record;
@@ -39,8 +37,33 @@ fn impl_record(input: &DeriveInput) -> proc_macro::TokenStream {
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
     let gen = quote! {
-        impl #impl_generics Record for #name #ty_generics #where_clause {
-            // ...
+        impl #impl_generics blockify::record::Record for #name #ty_generics #where_clause {
+            fn sign(&self, key: &blockify::AuthKeyPair) -> Result<blockify::DigitalSignature, blockify::SigningError> {
+                let msg = blockify::serialize(self)?;
+                let signature = blockify::sign_msg(&msg, key)?;
+                Ok(signature)
+            }
+
+            fn verify(&self, signature: &blockify::DigitalSignature, key: &blockify::PublicKey) -> Result<(), blockify::VerificationError> {
+                let msg = blockify::serialize(self).map_err(|_| blockify::VerificationError::SerdeError)?;
+                key.verify(&msg, signature)
+            }
+
+            fn record(self, keypair: blockify::AuthKeyPair, metadata: blockify::data::Metadata) -> Result<blockify::record::SignedRecord<Self>, blockify::SigningError> {
+                let signature = self.sign(&keypair)?;
+                let hash = self.hash();
+                Ok(blockify::record::SignedRecord::new(
+                    self,
+                    signature,
+                    keypair.into_public_key(),
+                    hash,
+                    metadata,
+                ))
+            }
+
+            fn hash(&self) -> blockify::Hash {
+                blockify::hash(self)
+            }
         }
     };
 
