e5a97fa83f54d4824b30ff419c2ba6642942847a
talentedKoda <59899139+talentedKoda@users.noreply.github.com>
2023-05-30 00:59:54 +0000

    untracked target

--- src/macros/record_derive/target/package/record_derive-0.2.2/target/debug/build/quote-5333412d78626f2c/build_script_build-5333412d78626f2c.d
+++
@@ -1,5 +0,0 @@
-/workspaces/blockify/src/macros/record_derive/target/package/record_derive-0.2.2/target/debug/build/quote-5333412d78626f2c/build_script_build-5333412d78626f2c: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.28/build.rs
-
-/workspaces/blockify/src/macros/record_derive/target/package/record_derive-0.2.2/target/debug/build/quote-5333412d78626f2c/build_script_build-5333412d78626f2c.d: /usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.28/build.rs
-
-/usr/local/cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.28/build.rs:
