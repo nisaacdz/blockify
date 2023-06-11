163642f79b23fccc9ee0ac160fa2b9392a47f4cd
nisaacdz <103804474+nisaacdz@users.noreply.github.com>
2023-06-11 17:46:39 +0000

    updated doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,115 @@
-//! # Blockify
+//! # BLOCKIFY
+//!
+//! [![Rayon crate](https://img.shields.io/crates/v/blockify.svg)](https://crates.io/crates/blockify)
+//! [![Rayon documentation](https://docs.rs/blockify/badge.svg)](https://docs.rs/blockify)
 //!
 //! A Rust blockchain library that provides the building blocks for creating a full-fledged blockchain application or platform, allowing you to focus on the higher-level features of your application without worrying about the low-level details of `block validation`, `data serialization`, `blockchain technology`, and `cryptographic operations`.
 //!
 //!
-//! This library provides concise API for:
+//! # USEFUL FEATURES
+//! 
+//! - Generating cryptographic key pairs
+//! - Signing data with cryptographic keys
+//! - Serializing data into bytes
+//! - Hashing different kinds of data
+//! - Building and blockchains
+//! - Building and deploying blocks into blockchains
+//! - Architecture for building fully-fledged blockchain applications
 //!
-//! - generating various `cryptographic keys`
-//! - `serializing`, `hashing`, and `signing` of different kinds of data
-//! - building and verifying `blocks`
-//! - building `blockchains` and appending `blocks` to them
-//! - creating and deploying `smart contracts`
-//! - building and managing `consensus protocols`
-//! - `merging of forked chains` based on consensus rules
+//!
+//! # USAGE
+//!
+//! 
+//! 
+//! - **Example**
+//!
+//! ```
+//! use blockify::{
+//! block::{LocalInstance, UnchainedInstance, Block},
+//! data::Metadata,
+//! record::{Record, SignedRecord}, SqliteChain, chain::Chain
+//! };
+//! use serde::{Deserialize, Serialize};
+//!
+//!    
+//! #[derive(Clone, Serialize, Deserialize, Record, Debug, PartialEq)]
+//! pub struct MarriageContract {
+//!     bride_name: String,
+//!        groom_name: String,
+//! }
+//!
+//! impl MarriageContract {
+//!     pub fn new(bride_name: &str, groom_name: &str) -> Self {
+//!         let (bride_name, groom_name) = (bride_name.to_owned(), groom_name.to_owned());
+//!             Self {
+//!                 bride_name,
+//!                 groom_name,
+//!             }
+//!     }
+//! 
+//!     pub fn generate() -> Self {
+//!         Self {
+//!             bride_name: "Julian".to_owned(),
+//!             groom_name: "Jolie".to_owned(),
+//!         }
+//!     }
+//! 
+//!     pub fn generate_records(amount: usize) -> Vec<SignedRecord<Self>> {
+//!         let mut res = Vec::with_capacity(amount);
+//!         (0..amount).for_each(|_| {
+//!             match Self::generate().record(blockify::generate_ed25519_key_pair(), Default::default()){
+//!                 Ok(v) => res.push(v),
+//!                 Err(_) => unreachable!("Error occurs")
+//!             }
+//!         });
+//!         res
+//!     }
+//!
+//! }
+//! ```
+//!
+//! 
+//! 
+//! - **Creating `Records` and `SignedRecords`**
+//!
+//! ```
+//! let contract = MarriageContract::new("John", "Julie");
+//! let keypair = blockify::generate_ed25519_key_pair();
+//! let signature = contract.sign(&keypair).unwrap();
+//! let hash = contract.hash();
+//! let record = contract.record(keypair, Metadata::empty()).unwrap();
+//! 
+//! assert_eq!(&hash, record.hash());
+//! assert_eq!(&signature, record.signature());
+//! assert!(record.verify().is_ok());
+//! ```
+//!
+//!
+//! 
+//! - **Assembling a `Block`**
+//! ```
+//! let mut pool = LocalInstance::new(Metadata::empty(), 0);
+//! let all_records = MarriageContract::generate_records(10);
+//! all_records.clone().into_iter().for_each(|record| pool.append(record).unwrap());
+//! ```
+//!
+//! 
+//! 
+//! - **`SqliteBlock` and `SqliteChain`**
+//! ```
+//! let chain_url = "target2/tests/marriagecontractchain/";
+//! std::fs::create_dir_all(chain_url).expect("could initialize directories");
+//! 
+//! let mut chain = SqliteChain::new(chain_url).unwrap();
+//! let position = chain.append(&pool).expect("Error appending to SqliteChain");
+//! let block = position.block(&chain).expect("Error getting block by position");
+//!
+//! assert_eq!(&all_records, &*block.records().expect("Error retrieving records from block"));
+//! ```
+//!
+//! 
+
+
 
 pub mod data;
 pub mod error;
