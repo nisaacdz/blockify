5579bc8c669d9e1aba9d2ccfc6101ef698f52e22
nisaacdz <103804474+nisaacdz@users.noreply.github.com>
2023-06-11 02:26:54 +0000

    added create_dir_all after initializing chain_url

--- tests/main_test.rs
+++ tests/main_test.rs
@@ -1,7 +1,13 @@
 #![cfg(test)]
 
-use blockify::{block::{LocalInstance, Block, UnchainedInstance}, record::Record, data::Metadata, SqliteChain, chain::Chain};
-use serde::{Serialize, Deserialize};
+use blockify::{
+    block::{Block, LocalInstance, UnchainedInstance},
+    chain::Chain,
+    data::Metadata,
+    record::Record,
+    SqliteChain,
+};
+use serde::{Deserialize, Serialize};
 
 #[test]
 fn test_blocks() {
@@ -71,36 +77,46 @@ fn test_blocks() {
         let records_from_block1 = block1
             .records()
             .expect("couldn't retrieve records from block1");
-        assert_eq!(builder1.records().unwrap().as_slice(), &*records_from_block1);
+        assert_eq!(
+            builder1.records().unwrap().as_slice(),
+            &*records_from_block1
+        );
 
         let records_from_block2 = block2
             .records()
             .expect("couldn't retrieve records from block2");
 
         // Assert the equality of the records from the two blocks
-        assert_eq!(builder1.records().unwrap().as_slice(), &*records_from_block1);
-        assert_eq!(builder2.records().unwrap().as_slice(), &*records_from_block2);
+        assert_eq!(
+            builder1.records().unwrap().as_slice(),
+            &*records_from_block1
+        );
+        assert_eq!(
+            builder2.records().unwrap().as_slice(),
+            &*records_from_block2
+        );
     }
     start()
 }
-
 
 #[test]
 fn test_2() {
     use blockify::{
         block::{LocalInstance, UnchainedInstance},
+        chain::Chain,
         data::Metadata,
-        record::{Record, SignedRecord}, SqliteChain, chain::Chain,
+        record::{Record, SignedRecord},
+        SqliteChain,
     };
     use serde::{Deserialize, Serialize};
-    
+
     // Deriving Record does the magic
     #[derive(Clone, Serialize, Deserialize, Record, Debug, PartialEq)]
     pub struct MarriageContract {
         bride_name: String,
         groom_name: String,
     }
-    
+
     impl MarriageContract {
         pub fn new(bride_name: &str, groom_name: &str) -> Self {
             let (bride_name, groom_name) = (bride_name.to_owned(), groom_name.to_owned());
@@ -109,18 +125,19 @@ fn test_2() {
                 groom_name,
             }
         }
-    
+
         pub fn generate() -> Self {
             Self {
                 bride_name: "Julian".to_owned(),
                 groom_name: "Jolie".to_owned(),
             }
         }
-    
+
         pub fn generate_records(amount: usize) -> Vec<SignedRecord<Self>> {
             let mut res = Vec::with_capacity(amount);
             (0..amount).for_each(|_| {
-                match Self::generate().record(blockify::generate_ed25519_key_pair(), Default::default())
+                match Self::generate()
+                    .record(blockify::generate_ed25519_key_pair(), Default::default())
                 {
                     Ok(v) => res.push(v),
                     Err(_) => unreachable!("Error occurs"),
@@ -129,28 +146,40 @@ fn test_2() {
             res
         }
     }
-    
+
     fn main() {
         let contract = MarriageContract::new("John", "Julie");
         let keypair = blockify::generate_ed25519_key_pair();
         let signature = contract.sign(&keypair).unwrap();
         let hash = contract.hash();
         let record = contract.record(keypair, Metadata::empty()).unwrap();
-    
+
         assert_eq!(&hash, record.hash());
         assert_eq!(&signature, record.signature());
         assert!(record.verify().is_ok());
-    
+
         let mut pool = LocalInstance::new(Metadata::empty(), 0);
         let all_records = MarriageContract::generate_records(10);
-        all_records.clone().into_iter().for_each(|record| pool.append(record).unwrap());
-    
-        let mut chain = SqliteChain::new("target2/tests/marriagecontractchain/").unwrap();
+        all_records
+            .clone()
+            .into_iter()
+            .for_each(|record| pool.append(record).unwrap());
+
+        let chain_url = "target2/tests/marriagecontractchain/";
+        std::fs::create_dir_all(chain_url).expect("could create directories");
+        let mut chain = SqliteChain::new(chain_url).unwrap();
         let position = chain.append(&pool).expect("Error appending to SqliteChain");
-        let block = position.block(&chain).expect("Error getting block by position");
-    
-        assert_eq!(&all_records, &*block.records().expect("Error retrieving records from block"));
+        let block = position
+            .block(&chain)
+            .expect("Error getting block by position");
+
+        assert_eq!(
+            &all_records,
+            &*block
+                .records()
+                .expect("Error retrieving records from block")
+        );
     }
-    
+
     main()
 }
