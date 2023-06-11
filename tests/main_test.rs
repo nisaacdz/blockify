d2eddb297642986545a43bf6271d88f12fa99d8a
nisaacdz <103804474+nisaacdz@users.noreply.github.com>
2023-06-11 01:48:58 +0000

    added new test function

--- tests/main_test.rs
+++ tests/main_test.rs
@@ -83,3 +83,74 @@ fn test_blocks() {
     }
     start()
 }
+
+
+#[test]
+fn test_2() {
+    use blockify::{
+        block::{LocalInstance, UnchainedInstance},
+        data::Metadata,
+        record::{Record, SignedRecord}, SqliteChain, chain::Chain,
+    };
+    use serde::{Deserialize, Serialize};
+    
+    // Deriving Record does the magic
+    #[derive(Clone, Serialize, Deserialize, Record, Debug, PartialEq)]
+    pub struct MarriageContract {
+        bride_name: String,
+        groom_name: String,
+    }
+    
+    impl MarriageContract {
+        pub fn new(bride_name: &str, groom_name: &str) -> Self {
+            let (bride_name, groom_name) = (bride_name.to_owned(), groom_name.to_owned());
+            Self {
+                bride_name,
+                groom_name,
+            }
+        }
+    
+        pub fn generate() -> Self {
+            Self {
+                bride_name: "Julian".to_owned(),
+                groom_name: "Jolie".to_owned(),
+            }
+        }
+    
+        pub fn generate_records(amount: usize) -> Vec<SignedRecord<Self>> {
+            let mut res = Vec::with_capacity(amount);
+            (0..amount).for_each(|_| {
+                match Self::generate().record(blockify::generate_ed25519_key_pair(), Default::default())
+                {
+                    Ok(v) => res.push(v),
+                    Err(_) => unreachable!("Error occurs"),
+                }
+            });
+            res
+        }
+    }
+    
+    fn main() {
+        let contract = MarriageContract::new("John", "Julie");
+        let keypair = blockify::generate_ed25519_key_pair();
+        let signature = contract.sign(&keypair).unwrap();
+        let hash = contract.hash();
+        let record = contract.record(keypair, Metadata::empty()).unwrap();
+    
+        assert_eq!(&hash, record.hash());
+        assert_eq!(&signature, record.signature());
+        assert!(record.verify().is_ok());
+    
+        let mut pool = LocalInstance::new(Metadata::empty(), 0);
+        let all_records = MarriageContract::generate_records(10);
+        all_records.clone().into_iter().for_each(|record| pool.append(record).unwrap());
+    
+        let mut chain = SqliteChain::new("target2/tests/marriagecontractchain/").unwrap();
+        let position = chain.append(&pool).expect("Error appending to SqliteChain");
+        let block = position.block(&chain).expect("Error getting block by position");
+    
+        assert_eq!(&all_records, &*block.records().expect("Error retrieving records from block"));
+    }
+    
+    main()
+}
